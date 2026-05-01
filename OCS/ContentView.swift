20e1892c1399c2cb678b4c549afaeebb03877422
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2026-05-02 00:03:27 +0200

    Initial Commit

---
+++ OCS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OCS
+//
+//  Created by Rodrigo Costa on 2026-05-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OCS/OCSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCSApp.swift
+//  OCS
+//
+//  Created by Rodrigo Costa on 2026-05-02.
+//
+
+import SwiftUI
+
+@main
+struct OCSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
